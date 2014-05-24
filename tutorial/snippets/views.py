7081231d2c99963ff14ed8f104ea10f755410b47
Shafique Jamal <shafique.jamal@gmail.com>
2014-05-23 23:39:27 -0400

    Tutorial 4: completed up to but NOT including 'Adding login to the Browsable API'

--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -3,17 +3,24 @@ from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer
 from snippets.serializers import UserSerializer
 from rest_framework import generics
-
+from rest_framework import permissions
 
 
 class SnippetList(generics.ListCreateAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
 
+    def pre_save(self, obj):
+        obj.owner = self.request.user
 
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+
+    def pre_save(self, obj):
+        obj.owner = self.request.user
 
 class UserList(generics.ListAPIView):
     queryset = User.objects.all()
