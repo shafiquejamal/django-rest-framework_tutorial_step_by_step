bcad37265acb29abce7231b05a04fcb3c9278fd3
Shafique Jamal <shafique.jamal@gmail.com>
2014-05-23 23:45:00 -0400

    Tutorial 4: Completed up to 'Adding login to the Browsable API' but not including 'Object level permissions'

--- tutorial/tutorial/urls.py
+++ tutorial/tutorial/urls.py
@@ -11,3 +11,8 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^', include('snippets.urls')),
 )
+
+urlpatterns += patterns('',
+    url(r'^api-auth/', include('rest_framework.urls',
+                               namespace='rest_framework')),
+)
