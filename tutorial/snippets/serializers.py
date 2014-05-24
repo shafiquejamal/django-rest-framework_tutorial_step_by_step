b663d3d2fb112936f088ab3b7712fc32f1bf27d5
Shafique Jamal <shafique.jamal@gmail.com>
2014-05-23 23:34:11 -0400

    Tutorial 4: 'Adding endpoints for our User models' but not including 'Associating Snippets with Users'

--- tutorial/snippets/serializers.py
+++ tutorial/snippets/serializers.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.forms import widgets
 from rest_framework import serializers
 from snippets.models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
@@ -6,3 +7,11 @@ class SnippetSerializer(serializers.ModelSerializer):
     class Meta:
         model = Snippet
         fields = ('id', 'title', 'code', 'linenos', 'language', 'style')
+
+
+class UserSerializer(serializers.ModelSerializer):
+    snippets = serializers.PrimaryKeyRelatedField(many=True)
+
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'snippets')
