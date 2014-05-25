35c02d994bddcfa198570fdae667f76ad8238464
Shafique Jamal <shafique.jamal@gmail.com>
2014-05-24 22:06:37 -0400

    Tutorial 5: completed 'Creating an endpoint for the highlighted snippets' but not including 'Hyperlinking our API'

--- tutorial/snippets/urls.py
+++ tutorial/snippets/urls.py
@@ -3,6 +3,8 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from snippets import views
 
 urlpatterns = patterns('',
+	url(r'^$', 'api_root'),
+	url(r'^snippets/(?P<pk>[0-9]+)/highlight/$', views.SnippetHighlight.as_view()),
     url(r'^snippets/$', views.SnippetList.as_view()),
     url(r'^snippets/(?P<pk>[0-9]+)/$', views.SnippetDetail.as_view()),
     url(r'^users/$', views.UserList.as_view()),
